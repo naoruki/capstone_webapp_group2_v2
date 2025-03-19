2eb1da75f7e3b7e500dbb78650636ba379925ee4
naoruki <72594954+naoruki@users.noreply.github.com>
2025-03-19 17:44:55 +0800

    made changes to dynamodb

--- flask-register-service/app.py
+++ flask-register-service/app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # AWS Configuration
 AWS_REGION = os.environ["AWS_REGION"]
-DYNAMODB_TABLE = os.environ["DYNAMODB_TABLE"]
+DYNAMODB_TABLE = os.environ.get("DYNAMODB_TABLE", "Users")
 dynamodb = boto3.resource("dynamodb", region_name=AWS_REGION)
 table = dynamodb.Table(DYNAMODB_TABLE)
 
