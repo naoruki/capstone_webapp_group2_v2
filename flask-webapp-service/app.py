a0e812141443b382341c760682a9a6c72fcea1cd
naoruki <72594954+naoruki@users.noreply.github.com>
2025-04-12 09:54:47 +0800

    new changes

--- flask-webapp-service/app.py
+++ flask-webapp-service/app.py
@@ -32,7 +32,7 @@ REGISTER_FORM = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Register</title>
+    <title>New Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
